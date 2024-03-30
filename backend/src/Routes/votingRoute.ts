df6ae9f58e6c1a9902a34bbec4d7c361fd8d6845
gabgaleski <galeski35@gmail.com>
2024-03-30 19:08:38 -0300

    fix: corrige modelo de banco de dados

--- backend/src/Routes/votingRoute.ts
+++ backend/src/Routes/votingRoute.ts
@@ -8,12 +8,15 @@ const votingController = new VotingController();
 
 router.get("/",
 (req: Request, res: Response) => votingController.getAll(req, res));
+
 router.post("/",
 VoteValidate.validateCreateVoting,
 (req: Request, res: Response) => votingController.create(req, res));
+
 router.put("/:id",
 VoteValidate.validateVote,
 (req: Request, res: Response) => votingController.vote(req, res));
+
 router.delete("/:id",
 (req: Request, res: Response) => votingController.delete(req, res));
 
